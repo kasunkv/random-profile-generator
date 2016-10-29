aaf84309e8626578394a8cb7df528ca82c4de3cf
kasunkv <kvkrusl@gmail.com>
2016-10-30 00:24:17 +0530

    refactor(ES6 Support): Convert source and test files to ES6
    Converted the index.js and index.test.js files to ES6. Updated the .tavis.yml to run new cover and
    
    build commands. Added dist and nyc_coverage folders to .gitignore
    
    closes #5

--- src/index.js
+++ src/index.js
@@ -1,68 +1,64 @@
-(function () {
-    'use strict';
+import Chance from 'chance';
+import moment from 'moment';
+import uniqueRandomArray from 'unique-random-array';
+import allNames from '../data/names.json';
+import allAddresses from '../data/addresses.json';  
 
-    var Chance = require('chance');
-    var moment = require('moment');
-    var uniqueRandomArray = require('unique-random-array');
-    var allNames = require('../data/names.json');
-    var allAddresses = require('../data/addresses.json');  
+let chance = new Chance();
+const avatarBase = 'https://api.adorable.io/avatars/200/';
+
+function profile() {
+    let profile = {};
+    let fullName = uniqueRandomArray(allNames)();
+    let bDay = moment(chance.birthday());
+    let today = moment(new Date());
+
+    profile.fullName = fullName;
+    profile.firstName = fullName.split(' ')[0];
+    profile.lastName = fullName.split(' ')[1];
+    profile.birthday = bDay.format('MMM Do[,] YYYY');
+    profile.age = today.diff(bDay, 'years');
+    profile.avatar = avatarBase + fullName.replace(/ /g, '-');
+    profile.address = uniqueRandomArray(allAddresses)();
+    profile.phone = chance.phone();
+    profile.email = chance.email();
+    profile.twitter = chance.twitter();
     
-    var chance = new Chance();
-    var avatarBase = 'https://api.adorable.io/avatars/200/';
+    return profile;
+}
 
-    function profile() {
-        var profile = {};
-        var fullName = uniqueRandomArray(allNames)();
-        var bDay = moment(chance.birthday());
-        var today = moment(new Date());
+function age() {
+    return chance.age();
+}
 
-        profile.fullName = fullName;
-        profile.firstName = fullName.split(' ')[0];
-        profile.lastName = fullName.split(' ')[1];
-        profile.birthday = bDay.format('MMM Do[,] YYYY');
-        profile.age = today.diff(bDay, 'years');
-        profile.avatar = avatarBase + fullName.replace(/ /g, '-');
-        profile.address = uniqueRandomArray(allAddresses)();
-        profile.phone = chance.phone();
-        profile.email = chance.email();
-        profile.twitter = chance.twitter();
-        
-        return profile;
-    }
+function birthday() {
+    return moment(chance.birthday()).format('MMM Do[,] YYYY');
+}
 
-    function age() {
-        return chance.age();
-    }
+function avatar() {
+    return avatarBase + uniqueRandomArray(allNames)().replace(/ /g, '-');
+}
 
-    function birthday() {
-        return moment(chance.birthday()).format('MMM Do[,] YYYY');
-    }
+function phone() {
+    return chance.phone();
+}
 
-    function avatar() {
-        return avatarBase + uniqueRandomArray(allNames)().replace(/ /g, '-');
-    }
+function email() {
+    return chance.email();
+}
 
-    function phone() {
-        return chance.phone();
-    }
+function twitter() {
+    return chance.twitter();
+}
 
-    function email() {
-        return chance.email();
-    }
-
-    function twitter() {
-        return chance.twitter();
-    }
-
-    module.exports = {
-        profile: profile,
-        randomName: uniqueRandomArray(allNames),
-        randomAddress: uniqueRandomArray(allAddresses),
-        randomBirthday: birthday,
-        randomAge: age,
-        randomAvatar: avatar,
-        randomPhone: phone,
-        randomEmail: email,
-        randomTwitter: twitter
-    };
-})();
+module.exports = {
+    profile: profile,
+    randomName: uniqueRandomArray(allNames),
+    randomAddress: uniqueRandomArray(allAddresses),
+    randomBirthday: birthday,
+    randomAge: age,
+    randomAvatar: avatar,
+    randomPhone: phone,
+    randomEmail: email,
+    randomTwitter: twitter
+};
